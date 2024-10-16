11b342ad771c003d345b0ec984fc084befd587a4
unknown <vi.nguyen2272@gmail.com>
2024-10-16 16:13:49 +0700

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
-
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import DefaultLayout from './components/defaultLayout/DefaultLayout'
 import SignUp from './pages/signup/SignUp'
 import Login from './pages/login/Login'
-// import Product from './components/products/Product'
-// import Contact from './pages/contact/Contact'
+
 
 function App() {
 
 
   return (
     <>
-    <BrowserRouter>
+    <Router>
     <Routes>
     <Route path="/*" element={<DefaultLayout />} />
     <Route path="/signup" element={<SignUp />} />
     <Route path="/login" element={<Login />} />
     </Routes>
-    </BrowserRouter>
-    {/* <Product/> */}
-     
-     {/* <Contact/> */}
+    </Router>
     </>
   )
 }
